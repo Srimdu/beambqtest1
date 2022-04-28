ab7629f0ea8fbc3fdd14b00fe2f295f32be93ef6
Srimdu <104509971+Srimdu@users.noreply.github.com>
2022-04-28 16:44:07 +0530

    Update code_staging.sh

--- code_staging.sh
+++ code_staging.sh
@@ -6,6 +6,10 @@ export BUCKET=${PROJECT_ID}
 
 pip3 install apache_beam[gcp]
 
+bq mk test_dataset
+
+bq mk test_dataset.weather Data_Precipitation:float,Date_Full:date,Date_Month:integer,Date_Week_of:integer,Date_Year:integer,Station_City:string,Station_Code:string,Station_Location:string,Station_State:string,Data_Temperature_Avg_Temp:integer,Data_Temperature_Max_Temp:integer,Data_Temperature_Min_Temp:integer,Data_Wind_Direction:integer,Data_Wind_Speed:float
+
 git clone https://github.com/Srimdu/beambqtest1
 
 gsutil mb gs://$PROJECT_ID
@@ -13,9 +17,5 @@ gsutil mb gs://$PROJECT_ID
 cd beambqtest1
 
 gsutil cp weather.csv gs://$PROJECT_ID/input/weather.csv
-
-bq mk test_dataset
-
-bq mk test_dataset.weather Data_Precipitation:float,Date_Full:date,Date_Month:integer,Date_Week_of:integer,Date_Year:integer,Station_City:string,Station_Code:string,Station_Location:string,Station_State:string,Data_Temperature_Avg_Temp:integer,Data_Temperature_Max_Temp:integer,Data_Temperature_Min_Temp:integer,Data_Wind_Direction:integer,Data_Wind_Speed:float
 
 python3 gcstobq.py  --project $PROJECT_ID --bucket $BUCKET
