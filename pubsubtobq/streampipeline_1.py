383450ba3d9a737d9950576174dbb772c390e84f
Srimdu <104509971+Srimdu@users.noreply.github.com>
2022-05-05 22:53:38 +0530

    Update streampipeline_1.py

--- pubsubtobq/streampipeline_1.py
+++ pubsubtobq/streampipeline_1.py
@@ -12,12 +12,30 @@ bq_schema = "Data_Precipitation:float,Date_Full:date,Date_Month:integer,Date_Wee
     Station_Location:string,Station_State:string,Data_Temperature_Avg_Temp:integer,Data_Temperature_Max_Temp:integer,Data_Temperature_Min_Temp:integer,\
     Data_Wind_Direction:integer,Data_Wind_Speed:float,process_timestamp:timestamp"
 
-class CustomParsing(beam.DoFn):
 
-    def process(self, element: bytes, timestamp=beam.DoFn.TimestampParam, window=beam.DoFn.WindowParam):
-        input_pc = json.loads(element.decode("utf-8"))
-        input_pc["timestamp"] = timestamp.to_rfc3339()
-        yield input_pc
+def json_parsing(data):
+    data1 = json.loads(data)
+    data["timestamp"] = '2008-12-25T03:30:00' #Hardcoded for now. In future it'll include process event timestamp
+    return data
+
+def convert_types(data):
+    #Converts string values to their appropriate type.
+    data['Data_Precipitation'] = float(data['Data_Precipitation']) if 'Data_Precipitation' in data else None
+    data['Date_Full'] = str(data['Date_Full']) if 'Date_Full' in data else None
+    data['Date_Month'] = int(data['Date_Month']) if 'Date_Month' in data else None
+    data['Date_Week_of'] = int(data['Date_Week_of']) if 'Date_Week_of' in data else None
+    data['Date_Year'] = int(data['Date_Year']) if 'Date_Year' in data else None
+    data['Station_City'] = str(data['Station_City']) if 'Station_City' in data else None
+    data['Station_Code'] = str(data['Station_Code']) if 'Station_Code' in data else None
+    data['Station_Location'] = str(data['Station_Location']) if 'Station_Location' in data else None
+    data['Station_State'] = str(data['Station_State']) if 'Station_State' in data else None
+    data['Data_Temperature_Avg_Temp'] = int(data['Data_Temperature_Avg_Temp']) if 'Data_Temperature_Avg_Temp' in data else None
+    data['Data_Temperature_Max_Temp'] = int(data['Data_Temperature_Max_Temp']) if 'Data_Temperature_Max_Temp' in data else None
+    data['Data_Temperature_Min_Temp'] = int(data['Data_Temperature_Min_Temp']) if 'Data_Temperature_Min_Temp' in data else None
+    data['Data_Wind_Direction'] = int(data['Data_Wind_Direction']) if 'Data_Wind_Direction' in data else None
+    data['Data_Wind_Speed'] = float(data['Data_Wind_Speed']) if 'Data_Wind_Speed' in data else None
+    return data
+
 
 def run(project,bucket,region,topic):
     argv = [
@@ -38,7 +56,9 @@ def run(project,bucket,region,topic):
         (
             p
             | "ReadFromPubSub" >> beam.io.gcp.pubsub.ReadFromPubSub(topic='projects/{0}/topics/{1}'.format(project,topic), timestamp_attribute=None)
-            | "MessageParse" >> beam.ParDo(CustomParsing)
+            | "UTF-8 bytes to string" >> beam.Map(lambda x: x.decode("utf-8"))
+            | "MessageParse" >> beam.Map(json_parsing)
+            | "Typeconversion" >> beam.Map(convert_types)
             | "WriteToBigQuery" >> beam.io.WriteToBigQuery(
             '{0}:test_dataset.weather_stream'.format(project),
             schema=bq_schema,
@@ -59,9 +79,5 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--topic', help='The Pubsub Topic', required=True)
 
     args = vars(parser.parse_args())
-    
-    print('Dataflow Job Started')
-    
+
     run(project=args['project'], bucket=args['bucket'], region=args['region'], topic=args['topic'])
-    
-    print('Job Completed')
